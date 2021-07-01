76fd52cf509449967f2a69be86b25202d1f01507
Cleber Miranda Barbosa <cleber@gmail.com>
2021-07-01 00:17:02 -0400

    Added ResquestMethod.GET

--- lesson_02_wiring_beans/src/main/java/com/example/lesson_06/spring/mvc/controller/SimpleController.java
+++ lesson_02_wiring_beans/src/main/java/com/example/lesson_06/spring/mvc/controller/SimpleController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Locale;
@@ -19,7 +20,7 @@ public class SimpleController {
         return "pong";
     }
 
-    @RequestMapping("/args")
+    @RequestMapping(value = "/args", method = RequestMethod.GET)
     @ResponseBody
     public Callable<ResponseEntity<String>> printArgs(Locale locale, @RequestHeader("User-Agent") String userAgent) {
         String originThread = Thread.currentThread().getName();
